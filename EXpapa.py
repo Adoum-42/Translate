b9da9ca995ff348f0ec1d95baff54cb3be36e681
Adoum-42 <151465005+Adoum-42@users.noreply.github.com>
2025-09-27 22:20:48 +0200

    Update EXpapa.py

--- EXpapa.py
+++ EXpapa.py
@@ -1,6 +1,6 @@
 #i = 0
 #while i < 10:
-from random import randint
+from random11 import randint
 motEsp = ["La narradora","La protagonista","El titulo","La autora","La lluvia","La niebla","El miedo","Bello","Un viajero"]
 motFr  = ["La naratrice","La protagoniste","Le titre","L'auteur","La pluie","Le brouillard","La peur","Beau","Un voyageur"]
 score = 0
@@ -13,4 +13,5 @@ for i in range (10):
         score += 1
         print ("essai N°",i+1)
 print("Score : ",score)
+
   
