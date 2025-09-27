f284ccb43528c75a62676726889f710b1641b181
Adoum-42 <151465005+Adoum-42@users.noreply.github.com>
2025-09-27 22:11:33 +0200

    Add files via upload

---
+++ EXpapa.py
@@ -0,0 +1,16 @@
+#i = 0
+#while i < 10:
+from random import randint
+motEsp = ["La narradora","La protagonista","El titulo","La autora","La lluvia","La niebla","El miedo","Bello","Un viajero"]
+motFr  = ["La naratrice","La protagoniste","Le titre","L'auteur","La pluie","Le brouillard","La peur","Beau","Un voyageur"]
+score = 0
+for i in range (10):
+    x=randint(0,8)
+ 
+    print(motEsp[x])
+    motUtil = input("Quelle est sa traduction ? ")
+    if motUtil==motFr[x] :
+        score += 1
+        print ("essai N°",i+1)
+print("Score : ",score)
+  
